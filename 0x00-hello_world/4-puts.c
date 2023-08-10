2d94ff46eb0677102005a91f6ec57eb93b0f2cd6
SOUKAINA <basmahajar@gmail.com>
2023-08-10 10:04:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
