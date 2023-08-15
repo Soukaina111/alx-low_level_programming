5098051a0f81978d27daf428d6295ed30c69708b
SOUKAINA <basmahajar@gmail.com>
2023-08-15 08:28:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
 
-int _putchar(char c)
-{
-    return write(1, &c, 1);
+int main(void) {
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return(0);
 }
