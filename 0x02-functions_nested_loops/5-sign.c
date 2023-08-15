6f8c741604353e6c2453b49d29fcf33da7e84460
SOUKAINA <basmahajar@gmail.com>
2023-08-15 11:23:59 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar('1' + '0');
-		return 1;
+		
 	}
 	else if (n == 0)
 	{
 		putchar('0');
-		return 0;
 	}
 	else
 	{
 		putchar('-');
-		putchar('1' + '0');
-		return -1;
+	
 	}
+
+	return n > 0 ? 1 : (n < 0 ? -1 : 0);
 }
