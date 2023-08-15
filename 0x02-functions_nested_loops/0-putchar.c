630482fb73eb17b37b5266fd06367d4836f0fed7
SOUKAINA <basmahajar@gmail.com>
2023-08-15 08:23:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,6 +4,18 @@ int _putchar(char c)
 {
     return write(1, &c, 1);
 }
-  
-    return (0);
+
+int main(void)
+{
+    char string[] = "_putchar";
+    int length = sizeof(string) - 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        _putchar(string[i]);
+    }
+
+    _putchar('\n');
+
+    return(0);
 }
