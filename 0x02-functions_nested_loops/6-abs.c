042a77d94e5bcd13081188a20c4b0b68bbc1a1bb
SOUKAINA <basmahajar@gmail.com>
2023-08-15 10:30:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @num: The integer value.
+ *
+ * Return: The absolute value of the number.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return -num;
+	else
+		return num;
+}
