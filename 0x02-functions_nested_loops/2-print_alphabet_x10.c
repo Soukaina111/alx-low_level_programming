cf89b81c0e697025ac100e3aa3c80961f8ab5fec
SOUKAINA <basmahajar@gmail.com>
2023-08-15 09:03:45 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,16 @@ void print_alphabet_x10(void)
 int count = 0;
 char letter = 'a';
 while (count < 10)
-   {
+{
 while (letter <= 'z')
-        {
+{
 putchar(letter);
 letter++;
-        }
+}
 
 count++;
 letter = 'a';/* Reset letter to 'a' for the next iteration */
 
 putchar('\n');
-    }
+}
 }
