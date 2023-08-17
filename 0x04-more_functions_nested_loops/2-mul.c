78d8fd6582491875809258ddbb59af3f7a21c6ff
SOUKAINA <basmahajar@gmail.com>
2023-08-17 13:03:18 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+	return result;
+}
