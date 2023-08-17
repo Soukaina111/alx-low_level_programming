1b65aff4fe4215277e32b47e2d4bf511834a392d
SOUKAINA <basmahajar@gmail.com>
2023-08-17 13:00:06 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1); /* Digit character */
+	}
+	else
+	{
+		return (0); /* Not a digit character */
+	}
+}
