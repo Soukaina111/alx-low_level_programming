444c16b01356f7acbefa84934134c12b7ce1d96d
SOUKAINA <basmahajar@gmail.com>
2023-08-17 13:36:05 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else
+		{
+			int num = i;
+
+			do {
+				putchar(num % 10 + '0');
+				num /= 10;
+			} while (num != 0);
+
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
