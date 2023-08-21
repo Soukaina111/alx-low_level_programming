473f0f1325ea2c5e67fca5b2a9cfd9e06170f376
SOUKAINA <basmahajar@gmail.com>
2023-08-21 09:13:51 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,19 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    // Calculate the length of the string
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    // Print the string in reverse
-    for (int i = length - 1; i >= 0; i--)
-    {
-        printf("%c", s[i]);
-    }
+	/* Print the string in reverse */
+	for (int i = length - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
 
-    printf("\n");
+	printf("\n");
 }
