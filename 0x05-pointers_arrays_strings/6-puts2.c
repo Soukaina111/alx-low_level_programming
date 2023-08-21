84f7bf0458004d3120aae6bc78e9a25e4ddbdd0b
SOUKAINA <basmahajar@gmail.com>
2023-08-21 09:22:24 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * puts2 - Prints every other character of a string,
+ *  starting with the first character,
+ *  followed by a new line.
+ * @str: Pointer to the string to be printed.
+ */
 void puts2(char *str)
 {
     int i = 0;
