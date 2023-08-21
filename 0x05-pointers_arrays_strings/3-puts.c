da6343f0756d113422dc2256292e5c68ce76dfb4
SOUKAINA <basmahajar@gmail.com>
2023-08-21 09:09:19 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,13 +6,13 @@
  */
 void _puts(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                write(1, &str[i], 1);
-                i++;
-        }
+	while (str[i] != '\0')
+	{
+        	write(1, &str[i], 1);
+        	i++;
+	}
 
-        write(1, "\n", 1);
+	write(1, "\n", 1);
 }
