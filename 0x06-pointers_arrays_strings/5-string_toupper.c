883e6d153fa31f45fe98fc36dc9009dd7be5d18c
SOUKAINA <basmahajar@gmail.com>
2023-08-23 08:14:26 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: Pointer to the string.
+ *
+ * Return: Pointer to the resulting string.
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+			*ptr = *ptr - ('a' - 'A');
+		ptr++;
+	}
+
+	return (str);
+}
