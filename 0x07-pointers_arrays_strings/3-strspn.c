2807540cae9fdbe9a1472dbccb851055b47f48b1
SOUKAINA <basmahajar@gmail.com>
2023-08-28 09:15:06 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,27 +9,29 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-	unsigned int count = 0;
-	int found;
+unsigned int i, j;
+unsigned int count = 0;
+int found;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		found = 0;
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				found = 1;
-				break;
-			}
-		}
+for (i = 0; s[i] != '\0'; i++)
+{
+found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
 
-		if (found)
-			count++;
-		else
-			break;
-	}
+if (found)
+{
+count++;
+}		
+else
+break;
+}
 
-	return (count);
+return (count);
 }
