c0353d62b744badc2652c31cb863d1b729f69084
SOUKAINA <basmahajar@gmail.com>
2023-08-30 14:29:22 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,3 +11,13 @@ int is_prime_number(int n)
 
 	return (is_prime_helper(n, 2));
 }
+int is_prime_helper(int n, int divisor)
+{
+	if (divisor * divisor > n)
+		return (1);
+
+	if (n % divisor == 0)
+		return (0);
+
+	return (is_prime_helper(n, divisor + 1));
+}
