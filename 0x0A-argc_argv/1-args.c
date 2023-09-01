a54654721b3b44a1fbbddc1c3ae3ffe91e77a4ab
SOUKAINA <basmahajar@gmail.com>
2023-09-01 12:01:10 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
-		
 
-		
 /**
-		
- * main - print the number of arguments passed to the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
  *
-		
- * Return: Always 0 (Success)
-		
+ * Return: 0 on success
  */
-		
-
-		
 int main(int argc, char *argv[])
-		
 {
-		
-(void) argv; /*Ignore argv*/
-		
 	printf("%d\n", argc - 1);
-		
-
-		
 	return (0);
-		
-
+}
