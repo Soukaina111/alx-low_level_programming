acae8166850a5c59b34d1ad1ae76e5ea90c009a6
SOUKAINA <basmahajar@gmail.com>
2023-09-21 09:24:33 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t s = 0;
+	size_t s = 0;
 
-for (; h; h = h->next, s++)
-    {
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-    }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
 
-return (s);
+	return (s);
 }
+
 
