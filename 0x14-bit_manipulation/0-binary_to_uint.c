59ce7b4c14809e71a9a70b051c66c245deb4b1d2
SOUKAINA <basmahajar@gmail.com>
2023-09-28 07:47:21 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,21 +13,17 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
-			return (0);
-		}
+		
+			return (0);		
 		else
-		{	
-			decimal_equivalent = 2 * decimal_equivalent + (b[i] - '0');
-		}
-	
-		}
+				
+		decimal_equivalent = 2 * decimal_equivalent + (b[i] - '0');
 
-
+	}
 
 
 	return (decimal_equivalent);
