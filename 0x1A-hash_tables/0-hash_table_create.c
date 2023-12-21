0b16f8217043c4fc74ead9a90511b7ef9da0b29b
Soukaina111 <basmahajar@gmail.com>
2023-12-21 07:56:05 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int cpt = 0;
 	hash_table_t *new = malloc(sizeof(hash_table_t));
-
 	if (new == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
@@ -24,12 +23,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
 	}
-
 	while (cpt < size) 
 	{
     	new->array[cpt] = NULL;
-    		cpt++;
+    	cpt++;
 	}
-
 	return (new);
 }
