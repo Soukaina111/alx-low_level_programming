7c4672143f09ee354914d0a3c9ffcdf603e05aad
Soukaina111 <basmahajar@gmail.com>
2023-12-21 08:12:23 -0800

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,18 +1,18 @@
 /**
- * hash_djb2 - takes a string and returnsan index number
- * @str: the string to be transformed to index
+ * hash_djb2 - takes a string and turns ito an index
+ * @str: the string to be hashed
  *
- * Return: The index number
+ * Return: The hashindex
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
