994d3f455fce0a4b4c17eab9fd5e242ea54dbaae
Soukaina111 <basmahajar@gmail.com>
2023-12-21 08:08:26 -0800

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,13 +6,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hashed;
-	int a;
+    unsigned long int hash;
+    int c;
 
-	hashed = 5381;
-	while ((a = *str++))
-	{
-		hashed = ((hashed << 5) + hashed) + a;
-	}
-	return (hash);
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
 }
