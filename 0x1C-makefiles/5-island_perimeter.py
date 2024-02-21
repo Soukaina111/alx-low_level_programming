f18b8f8861b4fe0cd1899f3b4b9446937ba84a47
Soukaina111 <basmahajar@gmail.com>
2024-02-21 07:31:54 -0800

    4-mmakefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,16 +4,42 @@ def island_perimeter(grid):
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
+    i = 1
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
+    while i < rows - 1:
+        j = 1
+        while j < cols - 1:
+            case = grid[i][j]
+            up = grid[i-1][j]
+            right = grid[i][j+1]
+            down = grid[i+1][j]
+            left = grid[i][j-1]
 
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+            count = 0
 
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
+            switch case:
+                case 1:
+                    count = up + right + down + left
+                    break
+                default:
+                    j += 1
+                    continue
+
+            switch count:
+                case 0:
+                    perimeter += 4
+                    break
+                case 1:
+                    perimeter += 3
+                    break
+                case 2:
+                    perimeter += 2
+                    break
+                case 3:
+                    perimeter += 1
+                    break
+
+            j += 1
+        i += 1
 
     return perimeter
