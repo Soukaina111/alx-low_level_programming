8ba7269fe4ab3089af6d6d8feda83ddbab490bdb
Soukaina111 <basmahajar@gmail.com>
2024-02-21 07:44:28 -0800

    4-mmakefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,39 +2,22 @@
 """ A FUNCTION that calculates  the perimeter of an island in shape of a grid"""
 
 def island_perimeter(grid):
-    """ this function:
-                    Args: grid, a list of list of integers
-                    Return: perimeter of the island described in grid
+    """shows the perimeter of the island
+    Args:
+         grid (list) = list of integers
     """
 
-    peri = 0
-    zone = []
+    x = len(grid[0])
+    y = len(grid)
+    z = 0
+    leng = 0
 
-    for items in range(len(grid)):
-        if items == 0 or items == len(grid):
-            continue
-        for cell in range(len(grid[items])):
-            if cell == 0 or cell == len(grid[items]):
-                continue
-            else:
-                if grid[items][cell] == 1:
-                    zone = [grid[items - 1][cell], grid[items][cell + 1],
-                                grid[items + 1][cell],  grid[items][cell - 1]]
-                    itera = 0
-                    for j in zone:
-                        if j == 0:
-                            itera = itera + 1
-                    if (itera == 4):
-                        return 4
-                    elif (itera == 3):
-                        peri = peri + 3
-                    elif (itera i == 2):
-                        peri = peri + 2
-                    elif (itera == 1):
-                        peri = peri + 1
-                    else:
-                        pass
-                    zone = []
-                else:
-                    pass
-    return peri
+    for i in range(y):
+        for j in range(x):
+            if grid[i][j] == 1:
+                leng += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    z += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    z+= 1
+    return leng * 4 - z * 2
