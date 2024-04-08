90e71dc06f24b055eb8306fb291c7434f777ebe2
Soukaina111 <basmahajar@gmail.com>
2024-04-07 23:34:06 -0700

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ * linear_search - This function looks  for a value in an array
+ * of integers using the Linear search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ * Return: The first index where the value is located
+ * or -1 if the value is not found or if the array is NULL.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked: i = %d, array[i] = %d\n", i, array[i]);
+		if (array[i] == value)
+	{
+		printf("Found %d at index: %zu\n", value, i);
+		return (i);
+	}
+	}
+	return (-1);
+}
+
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * This fucntion lookss for a value in a sorted array of integers using the Binary search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The index where the value is located, or -1 if the value is not found or if the array is NULL.
+ */
+int binary_search(int *array, size_t size, int value) {
+	int gauche = 0;
+    	int droit = size - 1;
+    	int half;
+	int i;
+
+	if (array == NULL) {
+        return (-1);
+    }
+    while (gauche <= droit) {
+         half = gauche + (droit - gauche) / 2;
+
+        printf("Searching in array: ");
+        for (i = gauche; i <= droit; i++) {
+            printf("%d, ", array[i]);
+        }
+        printf("\n");
+
+        if (array[half] == value) {
+            return half;
+        } else if (array[half] < value) {
+            gauche = half + 1;
+        } else {
+            droit = half - 1;
+        }
+    }
+
+    return (-1);
+}
+
