697af9238c7a90ae7f2dc7e51460014028bd7399
Soukaina111 <basmahajar@gmail.com>
2024-04-09 09:33:55 -0700

    DD

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - This funtion lookss for a value in a sorted
+ * binary_search - This function looks for a value in a sorted
  * array of integers using the Binary search algorithm.
  * @array: A pointer to the first element of the array to search in.
  * @size: The number of elements in the array.
@@ -12,41 +12,41 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int gauche = 0;
-	int droit =(int) size - 1;
-	int half;
-	int i;
+    int gauche = 0;
+    int droit = size - 1;
+    int half;
+    int i;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
-	while (gauche <= droit)
-	{
-		half = gauche + (droit - gauche) / 2;
-		printf("Searching in array: ");
-		for (i = gauche; i <= droit; i++)
-		{
-			printf("%d ", array[i]);
-			if (i != droit)
-			{
-				 printf(", ");
-
-			}
-		}
-		printf("\n");
-		if (array[half] == value)
-		{
-			return (half);
-		}
-		else if (array[half] < value)
-		{
-			gauche = half + 1;
-		}
-		else
-		{
-			droit = half - 1;
-		}
-	}
-	return (-1);
+    if (array == NULL)
+    {
+        return (-1);
+    }
+    while (gauche <= droit)
+    {
+        half = gauche + (droit - gauche) / 2;
+        printf("Searching in array: ");
+        for (i = gauche; i <= droit; i++)
+        {
+            printf("%d ", array[i]);
+            if (i != droit)
+            {
+                printf(", ");
+            }
+        }
+        printf("\n");
+        if (array[half] == value)
+        {
+            return (half);
+        }
+        else if (array[half] < value)
+        {
+            gauche = half + 1;
+        }
+        else
+        {
+            droit = half - 1;
+        }
+    }
+    return (-1);
 }
+
