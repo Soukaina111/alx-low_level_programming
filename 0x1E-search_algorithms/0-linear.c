878aff81c36782185575513db109cdad7fd332e4
Soukaina111 <basmahajar@gmail.com>
2024-04-09 08:41:00 -0700

    DONE

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,17 +11,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i <(int)size; i++)
 	{
 		printf("Value checked: i = %d, array[i] = %d\n", i, array[i]);
 		if (array[i] == value)
 	{
+		printf("Found %d at index: %d\n", value, i);
 		return (i);
 	}
 	}
