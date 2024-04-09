3af7aed7a0501555b196558a7ec5375ee5200ff5
Soukaina111 <basmahajar@gmail.com>
2024-04-09 09:53:11 -0700

    DD

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,41 +12,41 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    int gauche = 0;
-    int droit = size - 1;
-    int half;
-    int i;
+	int gauche = 0;
+	int droit = size - 1;
+	int half;
+	int i;
 
-    if (array == NULL)
-    {
-	    return (-1);
-    }
-    while (gauche <= droit)
-    {
-	    half = gauche + (droit - gauche) / 2;
-	    printf("Searching in array: ");
-	    for (i = gauche; i <= droit; i++)
-	    {
-		printf("%d", array[i]);
-		if (i != droit)
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (gauche <= droit)
+	{
+		half = gauche + (droit - gauche) / 2;
+		printf("Searching in array: ");
+		for (i = gauche; i <= droit; i++)
 		{
-			printf(", ");
+			printf("%d", array[i]);
+			if (i != droit)
+			{
+				printf(", ");
+			}
 		}
-	    }
-	printf("\n");
-	if (array[half] == value)
-	{
-		return (half);
+		printf("\n");
+		if (array[half] == value)
+		{
+			return (half);
+		}
+		else if (array[half] < value)
+		{
+			gauche = half + 1;
+		}
+		else
+		{
+			droit = half - 1;
+		}
 	}
-	else if (array[half] < value)
-	{
-		gauche = half + 1;
-	}
-	else
-	{
-		droit = half - 1;
-	}
-    }
-    return (-1);
+	return (-1);
 }
 
